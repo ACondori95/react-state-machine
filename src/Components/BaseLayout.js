576f9e571f7401d03027d460b83ef6e21c97f910
Álvaro Condorí <condorialvaro95@gmail.com>
2025-03-25 20:56:29 -0300

    Estados

--- src/Components/BaseLayout.js
+++ src/Components/BaseLayout.js
@@ -6,6 +6,9 @@ export const BaseLayout = () => {
   const [state, send] = useMachine(bookingMachine);
 
   console.log("nuestra maquina", state);
+  console.log("matched true", state.matches("inicial"));
+  console.log("matched false", state.matches("tickets"));
+  console.log("can", state.can("FINISH"));
 
   return <div>BaseLayout</div>;
 };
