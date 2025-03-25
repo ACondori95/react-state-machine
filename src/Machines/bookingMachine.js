04874ef26ae3f43389f6e4972ddf4a2d2b83c88b
Álvaro Condorí <condorialvaro95@gmail.com>
2025-03-25 19:19:08 -0300

    Declarar la maquina de estados

---
+++ src/Machines/bookingMachine.js
@@ -0,0 +1,45 @@
+import {createMachine} from "xstate";
+
+const bookingMachine = createMachine({
+  context: {},
+  id: "buy plane tickets",
+  initial: "inicial",
+  states: {
+    inicial: {
+      on: {
+        START: {
+          target: "search",
+        },
+      },
+    },
+    search: {
+      on: {
+        CONTINUE: {
+          target: "passengers",
+        },
+        CANCEL: {
+          target: "inicial",
+        },
+      },
+    },
+    passengers: {
+      on: {
+        DONE: {
+          target: "tickets",
+        },
+        CANCEL: {
+          target: "inicial",
+        },
+      },
+    },
+    tickets: {
+      on: {
+        FINISH: {
+          target: "inicial",
+        },
+      },
+    },
+  },
+});
+
+export default bookingMachine;
