33ea7910ce704f20eec4b2a8158fa32f1f3f9570
Álvaro Condorí <condorialvaro95@gmail.com>
2025-03-26 11:58:32 -0300

    Implementando los eventos y transiciones

---
+++ src/Components/Passengers.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import "./Passengers.css";
+
+export const Passengers = ({state, send}) => {
+  const [value, setValue] = useState("");
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const goToTicket = () => {
+    send({type: "DONE"});
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={submit} className='Passengers'>
+      <p className='Passengers-title title'>
+        Agrega a las personas que van a volar ✈️
+      </p>
+      <input
+        type='text'
+        name='name'
+        id='name'
+        placeholder='Escribe el nombre completo'
+        required
+        value={value}
+        onChange={onChangeInput}
+      />
+      <div className='Passengers-buttons'>
+        <button type='submit' className='Passengers-add button-secondary'>
+          Agregar Pasajero
+        </button>
+        <button
+          type='button'
+          className='Passenger-pay button'
+          onClick={goToTicket}>
+          Ver mi ticket
+        </button>
+      </div>
+    </form>
+  );
+};
