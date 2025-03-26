3849efc2c6721fe056bcbb7f90af79ec11ba0194
Álvaro Condorí <condorialvaro95@gmail.com>
2025-03-26 12:15:23 -0300

    Acciones

--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,45 +1,57 @@
 import {createMachine} from "xstate";
 
-const bookingMachine = createMachine({
-  context: {},
-  id: "buy plane tickets",
-  initial: "inicial",
-  states: {
-    inicial: {
-      on: {
-        START: {
-          target: "search",
+const bookingMachine = createMachine(
+  {
+    context: {},
+    id: "buy plane tickets",
+    initial: "inicial",
+    states: {
+      inicial: {
+        on: {
+          START: {
+            target: "search",
+            actions: "imprimirInicio",
+          },
         },
       },
-    },
-    search: {
-      on: {
-        CONTINUE: {
-          target: "passengers",
-        },
-        CANCEL: {
-          target: "inicial",
-        },
-      },
-    },
-    passengers: {
-      on: {
-        DONE: {
-          target: "tickets",
-        },
-        CANCEL: {
-          target: "inicial",
+      search: {
+        entry: "imprimirEntrada",
+        exit: "imprimirSalida",
+        on: {
+          CONTINUE: {
+            target: "passengers",
+          },
+          CANCEL: {
+            target: "inicial",
+          },
         },
       },
-    },
-    tickets: {
-      on: {
-        FINISH: {
-          target: "inicial",
+      passengers: {
+        on: {
+          DONE: {
+            target: "tickets",
+          },
+          CANCEL: {
+            target: "inicial",
+          },
+        },
+      },
+      tickets: {
+        on: {
+          FINISH: {
+            target: "inicial",
+          },
         },
       },
     },
   },
-});
+  {
+    actions: {
+      imprimirInicio: () => console.log("Imprimir inicio"),
+      imprimirEntrada: () => console.log("Imprimir entrada al search"),
+      imprimirSalida: () => console.log("Imprimir salida del search"),
+    },
+  }
+);
 
 export default bookingMachine;
